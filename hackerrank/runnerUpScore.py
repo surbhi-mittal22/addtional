de6da121f7bc29d929af122b52ef5a6225839b24
Surbhi Mittal <65413535+surbhi-mittal22@users.noreply.github.com>
2020-06-07 18:10:29 +0530

    Add files via upload

---
+++ hackerrank/runnerUpScore.py
@@ -0,0 +1,9 @@
+n = int(raw_input())
+    arr = map(int, raw_input().split())
+    largest = max(arr)
+
+for i in range(n):
+    if largest == max(arr):
+        arr.remove(max(arr))
+
+print(max(arr))
