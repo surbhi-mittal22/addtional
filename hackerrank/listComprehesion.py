8cff868c972fe54e726e73ad82ed7c74d96db00c
Surbhi Mittal <65413535+surbhi-mittal22@users.noreply.github.com>
2020-06-07 18:09:11 +0530

    Add files via upload

---
+++ hackerrank/listComprehesion.py
@@ -0,0 +1,9 @@
+x = int(raw_input())
+    y = int(raw_input())
+    z = int(raw_input())
+    n = int(raw_input())
+    print ([[a,b,c]
+     for a in range(0,x+1) 
+        for b in range(0,y+1) 
+           for c in range(0,z+1) 
+               if a + b + c != n ])
